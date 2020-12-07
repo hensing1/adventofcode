9885a8888ebf80bd315699e08b88dcfc0288c487
Henning Lehmann <henning.lehmann@gicom.org>
2020-12-07 17:47:01 +0100

    dec 7 (not implemented)

---
+++ 2020/days/7/Solver.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using _2020.Utility;
+using static _2020.Utility.Attributes;
+
+namespace _2020.days._7
+{
+    [ProblemDate(7)]
+    class Solver : ISolver
+    {
+        public string SolveFirst(string input)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string SolveSecond(string input)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
