01efd8eff945df8458e3abd45cdbf2fb429fe971
Henry Lehmann <34438247+henry-dv@users.noreply.github.com>
2020-12-15 01:12:04 +0100

    added InvalidInputException

---
+++ 2020/Utility/InvalidInputException.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace _2020.Utility
+{
+    class InvalidInputException : Exception
+    {
+        public InvalidInputException() : base("Invalid input file.")
+        {
+        }
+    }
+}
