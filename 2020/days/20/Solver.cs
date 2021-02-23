7bf0839fb1fc0dc6d26d7278b7250fe7f16b3edc
Henry Lehmann <34438247+henry-dv@users.noreply.github.com>
2021-02-24 00:44:37 +0100

    20 skeleton

---
+++ 2020/days/20/Solver.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using _2020.Utility;
+using static _2020.Utility.Attributes;
+
+namespace _2020.days._20
+{
+    [ProblemDate(20)]
+    class Solver : ISolver
+    {
+        public string SolveFirst(string input)
+        {
+            string[] lines = System.IO.File.ReadAllLines(input);
+            throw new NotImplementedException();
+        }
+
+        public string SolveSecond(string input)
+        {
+            string[] lines = System.IO.File.ReadAllLines(input);
+            throw new NotImplementedException();
+        }
+    }
+}
