cd761be874c9e17de7dbb8a6598120806c6e80bf
Henry Lehmann <34438247+henry-dv@users.noreply.github.com>
2021-04-12 00:52:25 +0200

    22

--- 2020/days/22/Solver.cs
+++ 2020/days/22/Solver.cs
@@ -31,7 +31,17 @@ namespace _2020.days._22
         public string SolveSecond(string input)
         {
             string[] lines = System.IO.File.ReadAllLines(input);
-            throw new NotImplementedException();
+
+            ReadInput(lines, out Queue<int> player1Deck, out Queue<int> player2Deck);
+
+            PlayGame(player1Deck, player2Deck, out int winner);
+
+            int result =
+                winner == 1 ?
+                    Tally(player1Deck) :
+                    Tally(player2Deck);
+
+            return result.ToString();
         }
 
         private void ReadInput(string[] lines, out Queue<int> player1Deck, out Queue<int> player2Deck)
@@ -82,13 +92,74 @@ namespace _2020.days._22
 
         private int Tally(Queue<int> deck)
         {
+            var deckClone = new Queue<int>(deck);
             int score = 0;
-            while (deck.Count > 0)
+            while (deckClone.Count > 0)
             {
-                score += deck.Peek() * deck.Count;
-                deck.Dequeue();
+                score += deckClone.Peek() * deckClone.Count;
+                deckClone.Dequeue();
             }
             return score;
         }
+
+        #region -- Recursive Area --
+
+        private void PlayGame(Queue<int> player1Deck, Queue<int> player2Deck, out int winner)
+        {
+            // keeping track of both deck histories in a single list, using longs to store two ints at once, is ridiculously slow for some reason
+            var previousP1Decks = new List<int>();
+            var previousP2Decks = new List<int>();
+
+            do
+            {
+                int p1Hash = Tally(player1Deck);
+                int p2Hash = Tally(player2Deck);
+                if (previousP1Decks.Contains(p1Hash) && previousP2Decks.Contains(p2Hash))
+                {
+                    winner = 1;
+                    return;
+                }
+                previousP1Decks.Add(p1Hash);
+                previousP2Decks.Add(p2Hash);
+            } while (PlayRound(player1Deck, player2Deck, out winner));
+        }
+
+        private bool PlayRound(Queue<int> player1Deck, Queue<int> player2Deck, out int winner)
+        {
+            int player1Card = player1Deck.Dequeue();
+            int player2Card = player2Deck.Dequeue();
+
+            if (player1Deck.Count >= player1Card && player2Deck.Count >= player2Card)
+            {
+                var newP1Deck = new Queue<int>(player1Deck.Take(player1Card));
+                var newP2Deck = new Queue<int>(player2Deck.Take(player2Card));
+
+                PlayGame(newP1Deck, newP2Deck, out winner);
+            }
+            else
+            {
+                if (player1Card > player2Card)
+                    winner = 1;
+                else
+                    winner = 2;
+            }
+
+            if (winner == 1)
+            {
+                player1Deck.Enqueue(player1Card);
+                player1Deck.Enqueue(player2Card);
+            }
+            else
+            {
+                player2Deck.Enqueue(player2Card);
+                player2Deck.Enqueue(player1Card);
+            }
+
+            if (player1Deck.Count == 0 || player2Deck.Count == 0)
+                return false;
+            return true;
+        }
+
+        #endregion
     }
 }
