f953e9fcd57a8934dd37c9abd9a826d751e59e45
Henry Lehmann <34438247+henry-dv@users.noreply.github.com>
2021-12-30 02:44:56 +0100

    2021 day 12

--- 2021/12/Solver.cs
+++ 2021/12/Solver.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
 using adventofcode.Utility;
 using static adventofcode.Utility.Attributes;
 
@@ -13,46 +9,141 @@ namespace adventofcode._2021._12
     {
         public string SolveFirst(string input)
         {
-            Node[] nodes = ParseInput(input);
-            throw new NotImplementedException();
+            MakeTree(input, out int numPaths);
+            return numPaths.ToString();
         }
 
         public string SolveSecond(string input)
         {
-            string[] lines = System.IO.File.ReadAllLines(input);
-            throw new NotImplementedException();
+            return MorePaths(input).ToString();
         }
 
-        private Node[] ParseInput(string input)
+        private Node MakeTree(string input, out int numPaths)
         {
             string[] lines = System.IO.File.ReadAllLines(input);
-            var connections = new (string, string)[lines.Length];
-            var nodes = new List<Node>();
+            var connections = new Dictionary<string, List<string>>();
+
             for (int i = 0; i < lines.Length; i++)
             {
                 var endpoints = lines[i].Split('-');
-                connections[i] = (endpoints[0], endpoints[1]);
-
-                foreach (string endpoint in endpoints)
-                    if (!nodes.Exists(node => node.Name == endpoint)) throw new NotImplementedException();
-                        //nodes.Add(new Node
-                        //{
-                        //    Name = endpoint
-                        //})
+                AddConnection(endpoints[0], endpoints[1], connections);
+                AddConnection(endpoints[1], endpoints[0], connections);
             }
 
-            throw new NotImplementedException();
+            numPaths = 0;
+            var startNode = new Node() { Name = "start", Visited = new() };
+            MakeSubTree(startNode, connections, ref numPaths);
+            return startNode;
+        }
+
+        private void AddConnection(string a, string b, Dictionary<string, List<string>> connections)
+        {
+            if (b == "start")
+                return;
+
+            if (connections.TryGetValue(a, out List<string> aConns))
+            {
+                if (!aConns.Contains(b))
+                    aConns.Add(b);
+            }
+            else
+                connections.Add(a, new List<string>() { b });
+        }
+
+        private void MakeSubTree(Node root, Dictionary<string, List<string>> connections, ref int numPaths)
+        {
+            var newVisited = new List<string>( root.Visited );
+            if (root.Name == root.Name.ToLower())
+                newVisited.Add(root.Name);
+
+            root.Descendants = new();
+            foreach (string conn in connections[root.Name])
+            {
+                if (!root.Visited.Contains(conn))
+                {
+                    var newDescendant = new Node() { Name = conn, Visited = new(newVisited) };
+                    root.Descendants.Add(newDescendant);
+                    if (conn == "end")
+                        numPaths++;
+                    else
+                        MakeSubTree(newDescendant, connections, ref numPaths);
+                }
+            }
         }
 
         struct Node
         {
             public string Name { get; init; }
-            public bool IsStart;
-            public bool IsEnd;
-            public bool IsBig;
-            public string[] Neighbors;
+            public List<Node> Descendants;
 
-
+            public List<string> Visited;
         }
+
+
+        #region Part Two
+
+        private int MorePaths(string input)
+        {
+            string[] lines = System.IO.File.ReadAllLines(input);
+            var connections = new Dictionary<string, List<string>>();
+
+            for (int i = 0; i < lines.Length; i++)
+            {
+                var endpoints = lines[i].Split('-');
+                AddConnection(endpoints[0], endpoints[1], connections);
+                AddConnection(endpoints[1], endpoints[0], connections);
+            }
+
+            var pathsUnderConsideration = new Stack<Path>(); // DFS faster than BFS because paths get removed faster
+
+            var initialPath = new Path { CurrentCave = "start", Visited = new() };
+            foreach (string cave in connections.Keys)
+                if (cave == cave.ToLower() && cave != "start")
+                    initialPath.Visited.Add(cave, 0);
+
+            int pathCount = 0;
+            pathsUnderConsideration.Push(initialPath);
+            while (pathsUnderConsideration.Count > 0)
+            {
+                Path currentPath = pathsUnderConsideration.Pop();
+                if (currentPath.Visited.ContainsKey(currentPath.CurrentCave))
+                    if (++currentPath.Visited[currentPath.CurrentCave] == 2)
+                        currentPath.SingleSmallCaveVisitedTwice = true;
+
+                foreach (string neighbor in connections[currentPath.CurrentCave])
+                {
+                    if (neighbor == "end")
+                        pathCount++;
+                    else if (currentPath.Visited.ContainsKey(neighbor))
+                    {
+                        if (currentPath.Visited[neighbor] < 1 || !currentPath.SingleSmallCaveVisitedTwice)
+                            pathsUnderConsideration.Push(new Path()
+                            {
+                                CurrentCave = neighbor,
+                                Visited = new(currentPath.Visited),
+                                SingleSmallCaveVisitedTwice = currentPath.SingleSmallCaveVisitedTwice
+                            });
+                    }
+                    else
+                        pathsUnderConsideration.Push(new Path()
+                        {
+                            CurrentCave = neighbor,
+                            Visited = new(currentPath.Visited),
+                            SingleSmallCaveVisitedTwice = currentPath.SingleSmallCaveVisitedTwice
+                        });
+                }
+            }
+
+            return pathCount;
+        }
+
+        struct Path
+        {
+            public Dictionary<string, int> Visited;
+            public string CurrentCave;
+            public bool SingleSmallCaveVisitedTwice;
+        }
+
+        #endregion
     }
 }
