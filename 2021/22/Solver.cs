f2e7072bf2bbfde038c7ccd68f1cd7da9dff1a11
Henry Lehmann <34438247+henry-dv@users.noreply.github.com>
2022-03-27 01:06:50 +0100

    2021 day 22 stub

---
+++ 2021/22/Solver.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace adventofcode._2021._22
+{
+    class Solver
+    {
+    }
+}
