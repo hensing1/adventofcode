503308f9ad142779c37d4740d821d6689ae42ed8
henry-dv <jenshenning.lehmann@rfh-campus.de>
2022-04-07 23:00:12 +0200

    2021 day 24 skeleton

---
+++ 2021/24/Solver.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using adventofcode.Utility;
+using static adventofcode.Utility.Attributes;
+
+namespace adventofcode._2021._24
+{
+    [ProblemDate(2021, 24)]
+    class Solver : ISolver
+    {
+        public string SolveFirst(string input)
+        {
+            string[] lines = System.IO.File.ReadAllLines(input);
+            throw new NotImplementedException();
+        }
+
+        public string SolveSecond(string input)
+        {
+            string[] lines = System.IO.File.ReadAllLines(input);
+            throw new NotImplementedException();
+        }
+
+        private enum Operation
+        {
+            Inp, Add, Mul, Div, Mod, Eql
+        }
+    }
+}
